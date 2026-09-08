6a47bcf3cb349c6d63aa4c77df16ef4c5b0092e3
1sl2tp <1sl2tp@gmail.com>
2026-09-08 11:35:06 +0700

    config: point GETLINK to Supabase Edge Function

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-window.GETLINK_API_BASE="https://getlink-api.taphoa-4ab8161d.workers.dev";
+window.GETLINK_API_BASE="https://gcnoahqsrquxkwkjbuxy.supabase.co/functions/v1/getlink-api";
+window.GETLINK_API_KEY="sb_publishable_UY3gfQ9MsntDFCUJ_uV0UA__eTYXz_w";

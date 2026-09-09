bbb60dd0c51906d7d89ac99b8ebb5ca45fc16cb1
1sl2tp <1sl2tp@gmail.com>
2026-09-09 11:32:19 +0700

    Invalidate stale supplier price cache

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function openUiCacheDb(){
     req.onerror=()=>reject(req.error);
   });
 }
-const UI_LIBRARY_CACHE_KEY="library-data-v4-supplier-pack";
+const UI_LIBRARY_CACHE_KEY="library-data-v5-supplier-price-basis";
 const UI_LIBRARY_CACHE_FALLBACK_KEYS=[];
 
 async function readUiLibraryCache(){

83f9cdaf69da5ae4cd7c0f4de2d11f3694f92215
1sl2tp <1sl2tp@gmail.com>
2026-09-10 15:38:03 +0700

    feat: load GETLINK order manager

--- config.js
+++ config.js
@@ -1,2 +1,25 @@
 window.GETLINK_API_BASE="https://gcnoahqsrquxkwkjbuxy.supabase.co/functions/v1/getlink-api";
 window.GETLINK_API_KEY="sb_publishable_UY3gfQ9MsntDFCUJ_uV0UA__eTYXz_w";
+
+(()=>{
+  const build=String(document.querySelector('meta[name="app-build-id"]')?.content||"dev");
+  const asset=name=>{
+    const url=new URL("./"+name,document.baseURI);
+    url.searchParams.set("v",build);
+    return url.toString();
+  };
+  if(!document.getElementById("getlinkOrderCss")){
+    const link=document.createElement("link");
+    link.id="getlinkOrderCss";
+    link.rel="stylesheet";
+    link.href=asset("order-management.css");
+    document.head.appendChild(link);
+  }
+  if(!document.getElementById("getlinkOrderJs")){
+    const script=document.createElement("script");
+    script.id="getlinkOrderJs";
+    script.src=asset("order-management.js");
+    script.async=false;
+    document.body.appendChild(script);
+  }
+})();
